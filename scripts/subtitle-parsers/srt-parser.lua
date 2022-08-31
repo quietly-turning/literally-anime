0a9fb96b6f76478a26e1fae65b95ef102f0f1693
quietly-turning <quietly.turning@gmail.com>
2022-08-30 23:10:04 -0400

    wip: srt-parser

--- scripts/subtitle-parsers/srt-parser.lua
+++ scripts/subtitle-parsers/srt-parser.lua
@@ -5,45 +5,64 @@ local helpers   = dofile(base_path.."scripts/AnimeHelpers.lua")
 
 local RageFile =
 {
-  READ       = 1,
-  WRITE      = 2,
-  STREAMED   = 4,
-  SLOW_FLUSH = 8,
+   READ       = 1,
+   WRITE      = 2,
+   STREAMED   = 4,
+   SLOW_FLUSH = 8,
 }
 
 local ParseFile = function( file_path )
-  local file = RageFileUtil.CreateRageFile()
+   local file = RageFileUtil.CreateRageFile()
 
-  if not file:Open(file_path, RageFile.READ) then
-    lua.ReportScriptError( string.format("ReadFile(%s): %s",file_path,file:GetError()) )
-    file:destroy()
-    return { }  -- return a blank table
-  end
+   if not file:Open(file_path, RageFile.READ) then
+      lua.ReportScriptError( string.format("ReadFile(%s): %s",file_path,file:GetError()) )
+      file:destroy()
+      return { }  -- return a blank table
+   end
 
-  local tbl = { }
-  local current = tbl
+   local events = {}
+   local line_num = 0
+   
+   while not file:AtEOF() do
+      local line = file:GetLine()
+      line_num = line_num+1
+      
+      if line ~= "" then
+         if tonumber(line) ~= nil then
+            line = file:GetLine()
+            line_num = line_num+1
+            
+            -- standardize numeric localization
+            line = line:gsub(",", ".")
+            
+            if line:match("%d+:%d+:%d+%.%d+ %-%-> %d+:%d+:%d+%.%d+") then
+               
+               local start, finish = line:match("(%d+:%d+:%d+%.%d+) %-%-> (%d+:%d+:%d+%.%d+)")
+               
+               local text = ""
 
-  while not file:AtEOF() do
-    local line = file:GetLine()
-    -- TODO: implement 😩
-  end
+               line = file:GetLine()
+               line_num = line_num+1
+               
+               while line ~= "" do
+                  text = text..line
+                  line = file:GetLine()
+                  line_num = line_num+1
+               end
+               
+               events[#events+1] = {Start=start, End=finish, Text=text}
+               
+            else
+               lua.ReportScriptError( ("Error parsing %s\n   line %d: Couldn't parse start and finish time."):format(file_path, line_num) )
+            end
+         end
+      end  
+   end
 
-  file:Close()
-  file:destroy()
-
-  -- if tbl.events and type(tbl.events)=="table" then
-  --   if tbl.events.Data and #tbl.events.Data > 0 then
-  --     -- tbl.events.Data is the relevant data we want to return to SM5
-  --     return tbl.events.Data
-  --
-  --   else
-  --     lua.ReportScriptError(("Subtitle parsing error:\n'[Events]' section successfully found in %s, but dialogue lines not parsed correctly.\n\n"):format(file_path))
-  --     return false
-  --   end
-  -- end
-  --
-  -- lua.ReportScriptError(("Subtitle parsing error:\n'[Events]' section could not be found in %s.\n\n"):format(file_path))
-  -- return false
+   file:Close()
+   file:destroy()
+   
+   return events
 end
 
 return ParseFile
